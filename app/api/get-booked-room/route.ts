6dfc21b1ee8a45d6789cd593eb4d63ed7894e0c6
Brian Aditya Dharmatirta <briiiad.dh@gmail.com>
2024-07-02 08:37:44 +0700

    fix: dynamic api

--- app/api/get-booked-room/route.ts
+++ app/api/get-booked-room/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
-
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   // Get all booked rooms
   const bookedRooms = await prisma.pinjam_Ruang.findMany({
